da160ee6dca10dad72716ddc25c8b920b8211927
DCOI <yang_ink@126.com>
2015-02-17 09:25:05 +0800

    Create include_std.h

---
+++ lib/include_std.h
@@ -0,0 +1,59 @@
+#include <malloc.h>
+#include <tchar.h> 
+#include <wchar.h> 
+#include  <fstream>
+#include<windows.h> 
+#include<iostream>
+#include<time.h>
+#include<stdio.h>
+#include<cstring>
+#include<cstdlib>
+#include<dir.h> 
+#include<iostream>
+#include<conio.h>
+
+void putitexit_to_manage();
+void outfuck();
+void fuckpingceset(int perm);
+void startfuckpingce();
+void fileopne();
+void filesystemview();
+void filemanroot();
+void fileman();
+void reload();
+void logoview();
+void restarting();
+void changepwdshow();
+void verpwd();
+void changepwdr();
+void shutdnlipro();
+int yesornot();
+void shutdownliteview(); 
+void pdperm();
+void setnotexist();
+void setback();
+void permnoteno();
+void systemdoview(int pid);
+void checkpasswd();
+void clrmode();
+void newsetpasswd();
+void alreadyroot();
+void inputpasswd();
+int EncryptFile();
+void OutputFile();
+int inputroot();
+void indexoperate();
+int DecryptFile();
+int inputcomm();
+void putitexit();
+void notexist();
+void getrootper(); 
+void startcheck();
+void startview();
+void indexoperate();
+void startscreen();
+void startscreenview();
+void startprogram();
+void helpscreen(); 
+void exitsystem();
+void inputlongword();
