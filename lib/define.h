3a5d5891cbeddf8913479de133ba7de012c40236
DCOI <yang_ink@126.com>
2015-02-17 09:23:00 +0800

    Create define.h

---
+++ lib/define.h
@@ -0,0 +1,16 @@
+#include "global.h"
+const int MAXWD=400;
+char username[MAXWD]=".USER_DCOI";
+char* fileuser;
+char* nowpwd;
+char versionseen[40]=_VERSION_;
+char rootkey[MAXWD]="s2*(23j1@ed3";
+char cdkey[MAXWD]="1099-2134-3678-2831";
+int sys_round_crypt_key_1[]={3,7,8,2,9,5,4};
+char f_extlist_ini[20]="ext/list.ini";
+char f_temp_dat[20]="temp.dat";
+char f_lang_cn_ini[20]="lang/lang_cn.ini";
+char f_lang_en_ini[20]="lang/lang_en.ini";
+char f_lang_jp_ini[20]="lang/lang_jp.ini";
+char f_lang_fr_ini[20]="lang/lang_fr.ini";
+char f_lang_ot_ini[20]="lang/lang_ot.ini";
